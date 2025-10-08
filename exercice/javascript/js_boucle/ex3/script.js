df5b8e0bd2cf9b07a24dedf693885f01d7960ec8
Lucas Q <lucas.queveau44190@gmail.com>
2025-10-08 11:25:42 +0200

    js_boucle ex3

---
+++ exercice/javascript/js_boucle/ex3/script.js
@@ -0,0 +1,14 @@
+const number = prompt("Choisissez un nombre entre 0 et 10");
+
+switch (true) {
+  case number > 10:
+    console.log("Vous avez rentrer un nombre superieur a 10");
+    break;
+  case number < 0:
+    console.log("Vous avez rentrez un nombre inferieur a 0");
+    break;
+}
+
+for(i = number; i > -1; i--){
+    console.log(i)
+}
