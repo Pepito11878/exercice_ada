7fd6f070780b150a9aaadf1fd2ddf08331a888ad
Lucas Q <lucas.queveau44190@gmail.com>
2025-10-08 14:06:23 +0200

    Update script.js

--- exercice/javascript/js_var/ex1/script.js
+++ exercice/javascript/js_var/ex1/script.js
@@ -6,11 +6,11 @@ const price2 = 20
 let total = price1 + price2
 // Prix d'un troisieme article
 const price3 = 50
-// Nous ajoutons a3 au total
+// Nous ajoutons le troisieme prix au total
 total = total + price3
 
 console.log("Le prix total avant reduction est de : ", total)
 
 //Nous appliquons 20% de reduction
-const tot2 = total * (1 - 20 / 100);
+const tot2 = total * (1 - 20 / 100); //calcul de -20% au total
 console.log("Le prix total après reduction est de : ", tot2)
