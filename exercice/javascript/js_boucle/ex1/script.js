619a3bde35e48e3deaed6ff08403538c72cae7f8
Lucas Q <lucas.queveau44190@gmail.com>
2025-10-08 10:38:06 +0200

    ex1 js_boucle

---
+++ exercice/javascript/js_boucle/ex1/script.js
@@ -0,0 +1,23 @@
+// 1
+// for(i = 0; i < 100; i++){
+//     console.log(i)
+// }
+
+// 2
+// let i = 0;
+// while (i < 99){
+//     i++;
+//     console.log(i)
+// }
+
+// 3 REFLEXION : la boucle la plus approprié est la boucle for
+
+// 4
+// for(i = 50; i < 100; i++){
+//     console.log(i)
+// }
+
+// 5
+for(i = 49; i > 0; i--){
+    console.log(i)
+}
