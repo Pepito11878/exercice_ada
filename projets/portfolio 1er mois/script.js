6d3291847137c7151b835752b14f1721105a5a98
Lucas Q <lucas.queveau44190@gmail.com>
2025-10-22 14:26:14 +0200

    changement js portfolio

---
+++ projets/portfolio 1er mois/script.js
@@ -0,0 +1,13 @@
+const mail = document.querySelector(".mail");
+const discord = document.querySelector(".discord");
+
+discord.addEventListener("click", () => {
+    discord.innerText = "pepito_11";
+    discord.style.textTransform = "none";
+});
+
+mail.addEventListener("click", () => {
+  mail.innerText = "lucas.queveau44190@gmail.com";
+  mail.style.textTransform = "none"
+});
+
