c2579db45fb82c5c20443ed6e94d2f2532659d83
Lucas Q <lucas.queveau44190@gmail.com>
2025-10-23 14:49:48 +0200

    correction exercice 2 de js_boucle

--- exercice/javascript/js_boucle/ex2/script.js
+++ exercice/javascript/js_boucle/ex2/script.js
@@ -1,9 +1,16 @@
-let message = ""
+let message = "";
+let message2 = "";
 
-let letters = ['A', 'd', 'a', '-', 'T', 'e', 'c', 'h']
+let letters = ["A", "d", "a", "-", "T", "e", "c", "h"];
+let letters2 = ["A", "d", "a", "-", "S", "c", "h", "o", "o", "l"];
 
-for(i = 0; i < letters.length; i++){
-    message += letters[i];
+for (i = 0; i < letters.length; i++) {
+  message += letters[i];
 }
 
-console.log(message)
+for (i = 0; i < letters2.length; i++) {
+  message2 += letters2[i];
+}
+
+console.log(`le premier message est ${message}`);
+console.log(`le second message est ${message2}`);
