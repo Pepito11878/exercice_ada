e841f6697d2ea1059b89b371b8ccda6f6e8da2dc
Lucas Q <lucas.queveau44190@gmail.com>
2025-10-23 15:23:28 +0200

    correction

--- exercice/javascript/js_condition/ex1/script.js
+++ exercice/javascript/js_condition/ex1/script.js
@@ -2,8 +2,10 @@
 // const variable = -8;
 const variable = 0;
 
-if (variable >= 0) {
-  alert(`${variable} est positif`);
-} else if(variable < 0){
-  alert(`${variable} est négatif`);
+if (variable < 0) {
+  alert(`${variable} est negatif`);
+} else if(variable === 0){
+  alert(`${variable} est postif`);
+}else{
+  alert(`${variable} est positif`)
 }
