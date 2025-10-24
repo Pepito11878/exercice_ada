7339ed38b8bc4c9c1e8c35f94b9175e849487809
Lucas Q <lucas.queveau44190@gmail.com>
2025-10-24 12:33:51 +0200

    new react tsx test

---
+++ lecon/react_graven/my-app/src/screens/Contact.tsx
@@ -0,0 +1,9 @@
+export const Contact = () => {
+    return(
+        <main>
+            <h3 className="font-extrabold text-3xl mb-4">Contact</h3>
+
+            <p>EMAIL : pqpito11878@gmail.com</p>
+        </main>
+    )
+}
---
+++ lecon/react_graven/my-app/src/screens/Home.tsx
@@ -0,0 +1,13 @@
+import { About } from "../components/About";
+import { YoutubeVideo } from "../components/YoutubeVideo";
+import { SocialLinks } from "../components/SocialLinks";
+
+export const Home = () => {
+  return (
+    <main>
+      <About/>
+      <YoutubeVideo id="yHoI0n2VxMU"/>
+      <SocialLinks/>
+    </main>
+  );
+};
