7d8254070b727b79adefbc76d1693a04af6b7811
Lucas Q <lucas.queveau44190@gmail.com>
2025-10-24 16:24:40 +0200

    some test on react nextjs

--- lecon/react_graven/my-app/src/components/About.tsx
+++
@@ -1,8 +0,0 @@
-export const About = () => {
-  return (
-    <section className="mb-5">
-      <h1 className="text-3xl font-extrabold">Bienvenue</h1>
-      <p className="mt-5">Texte</p>
-    </section>
-  );
-};
--- lecon/react_graven/my-app/src/screens/Contact.tsx
+++
@@ -1,9 +0,0 @@
-export const Contact = () => {
-    return(
-        <main>
-            <h3 className="font-extrabold text-3xl mb-4">Contact</h3>
-
-            <p>EMAIL : pqpito11878@gmail.com</p>
-        </main>
-    )
-}
