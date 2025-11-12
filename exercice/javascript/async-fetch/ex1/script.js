5378b4a585060fc9b2f76043187c3244e4eceea7
Lucas Q <lucas.queveau44190@gmail.com>
2025-11-12 10:23:39 +0100

    fetch ex1

---
+++ exercice/javascript/async-fetch/ex1/script.js
@@ -0,0 +1,14 @@
+const element = document.querySelector(".element");
+
+async function fetchOffers() {
+  const response = await fetch(
+    "https://corsproxy.io/?https://codepassport.dev/api/offers"
+  );
+  const offers = await response.json();
+
+  element.innerHTML = offers.map(offers => `
+      <h2>${offers.titre}</h2>
+      <p>${offers.description}</p>
+    `).join("");
+}
+fetchOffers();
