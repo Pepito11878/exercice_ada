80cc563771ab79161a3f0ec9a9bce9af12429074
Lucas Q <lucas.queveau44190@gmail.com>
2025-11-14 15:52:37 +0100

    .

--- exercice/javascript/cleancode/ex4/script.js
+++ exercice/javascript/cleancode/ex4/script.js
@@ -6,26 +6,25 @@ const cities = [
   { name: "lille", lat: 50.633333, lon: 3.066667 },
 ];
 
-async function fetchWeather(city) {
-  for (let i = 0; i < cities.length; i++) {
-    if (city !== cities[i].name) {
-      console.error("invalid city");
-    } else {
-      let resp = await fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${cities[i].lat}&longitude=${cities[i].lon}&current=temperature_2m,rain`
-      );
+async function fetchWeather(cityName) {
+  try {
+    const city = cities.find((c) => c.name === cityName);
+    if (!city) return console.error("Ville inconnue :", cityName);
 
-      let json = await resp.json();
+    const resp = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${city.lat}&longitude=${city.lon}&current=temperature_2m,rain`
+    );
 
-      console.log(json.current.rain);
+    const json = await resp.json();
 
-      document.getElementById(
-        `pluie_${city}`
-      ).innerHTML = `<strong>"${json.current.rain}</strong>`;
-      document.getElementById(
-        `degres_${city}`
-      ).innerHTML = `<strong>${json.current.temperature_2m}</strong>`;
-    }
+    document.getElementById(`pluie_${cityName}`).innerHTML =
+      `<strong>${json.current.rain}</strong>`;
+
+    document.getElementById(`degres_${cityName}`).innerHTML =
+      `<strong>${json.current.temperature_2m}</strong>`;
+
+  } catch (error) {
+    console.log(error);
   }
 }
 
