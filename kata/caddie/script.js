1d1ba39b70ae28813af815f3924eba153c3197c3
Lucas Q <lucas.queveau44190@gmail.com>
2026-02-09 12:28:12 +0100

    update script.js

--- kata/caddie/script.js
+++ kata/caddie/script.js
@@ -1,8 +1,8 @@
-const caddie = [{ name: "bananas", price: 1.2, quantity: 3 }];
+const items = [{ name: "bananas", price: 1.2, quantity: 3 }];
 
 function calculateTotal(cart) {
     for(let i = 0; i < caddie.length; i++){
         const total = caddie[i].price * caddie[i].quantity
-        
+
     }
 }
